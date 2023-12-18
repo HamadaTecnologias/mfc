0cf43bfc1f6dca61a1c9e0b94c64904d16568ef5
HamadaTecnologias <hamada.st@hotmail.com>
2023-12-18 01:26:37 -0300

    inicio

---
+++ login/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+    $nome = $_POST['nome'];
+    $senha = $_POST['senha'];
+    echo " $nome | $senha";
+
+    if($nome == 'login' && $senha == '1234'){
+        session_start();
+        $_SESSION['nome'] = $nome;
+        header('location:../fornecedores.html');
+
+    }else{
+
+        header('location:../index.html?erro_pass=true');
+        exit;
+    }
+
+?>
